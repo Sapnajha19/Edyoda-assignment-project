622af6318b513e419f25282e6fce138a5edcf652
Sapna Jha <sapnajha1919@gmail.com>
2023-04-23 23:23:55 +0530

    Fix connected border between divs

--- src/components/Modules/Topic/Topic.js
+++ src/components/Modules/Topic/Topic.js
@@ -12,26 +12,26 @@ export default function Topic() {
                     <p style={{ color: "#5f5c5c" }}>15 December 2021, Wednesday, 07:30 PM</p>
                 </div>
                 <p style={{ color: "#0d8dfa", fontSize: "14px", fontWeight: "bold", paddingLeft: "800px" }}>DAILY FEEDBACK</p>
-                <div className='a' style={{ display: "flex", flexDirection: "row", gap: "20px", justifyContent: "center", paddingRight: "41px" }}>
-                    <div style={{ background: "white", borderTop: "1px solid #c6ebf5", borderLeft: "1px solid #c6ebf5", borderRight: "1px solid #c6ebf5", borderRadius: "4px 4px 0 0", display: "flex", flexDirection: "row", alignItems: "center", boxShadow: "2px 0 0 0 rgba(0, 0, 0, 0.1)", paddingLeft: "15px", paddingRight: "15px" }}>
+                <div className='a' style={{ display: "flex", flexDirection: "row", justifyContent: "center", paddingRight: "2px" }}>
+                    <div style={{ background: "white", borderTop: "1px solid #a4cae9", borderLeft: "1px solid #a4cae9", borderRight: "1px solid #a4cae9", borderRadius: "4px 4px 0 0", display: "flex", flexDirection: "row", alignItems: "center", boxShadow: "2px 0 0 0 rgba(0, 0, 0, 0.1)", paddingLeft: "18px", paddingRight: "15px" }}>
                         <img src={informationCircle1} alt="info-icon" style={{ height: "20px", width: "20px" }} />
                         <p style={{ fontWeight: "bold", fontSize: "14px" }}>SESSION PLAN</p>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" , borderBottom:"1px solid #a4cae9" , paddingLeft: "18px", paddingRight: "15px"}}>
                         <img src={topic} alt="video=icon" style={{ height: "20px", width: "20px" }} />
                         <p style={{ color: "#5f5c5c", fontWeight: "bold", fontSize: "14px" }}>PRE-WATCH VIDEOS</p>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" , borderBottom:"1px solid #a4cae9" , paddingLeft: "18px", paddingRight: "15px"}}>
                         <img src={videocam} alt="" style={{ height: "20px", width: "20px" }} />
                         <p style={{ color: "#5f5c5c", fontWeight: "bold", fontSize: "14px" }}>SESSION RECORDING</p>
                     </div>
-                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                    <div style={{ display: "flex", flexDirection: "row", alignItems: "center" , borderBottom:"1px solid #a4cae9" , paddingLeft: "18px", paddingRight: "15px"}}>
                         <img src={document} alt="" style={{ height: "20px", width: "20px" }} />
                         <p style={{ color: "#5f5c5c", fontWeight: "bold", fontSize: "14px" }}>REFERENCES</p>
                     </div>
                 </div>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <div style={{ background: "#ffffff", height: "400px", width: "700px", borderLeft: "1px solid #c6ebf5", borderRight: "1px solid #c6ebf5", borderBottom: "1px solid #c6ebf5", borderTop: "1px solid #c6ebf5", borderRadius: "0 4px 4px 4px", boxSizing: "border-box", paddingLeft: "50px" }}>
+                    <div style={{ background: "#ffffff", height: "400px", width: "700px", borderLeft: "1px solid #a4cae9", borderRight: "1px solid #a4cae9", borderBottom: "1px solid #a4cae9", borderRadius: "0 4px 4px 4px", boxSizing: "border-box", paddingLeft: "50px" }}>
                         <p style={{ fontWeight: "bold", fontSize: "20px" }}>Session Plan</p>
                         <p style={{ color: "#5f5c5c" }}>LIVE session is about to start. Please stay tuned.</p>
                         <button style={{ padding: "10px", borderRadius: "15px", border: "none", fontSize: "12px", fontWeight: "semibold" }}>JOIN LIVE SESSION</button>
