2d22704d8003c13a54e6670cadaed381ceff4cbb
Sapna Jha <sapnajha1919@gmail.com>
2023-04-23 18:08:50 +0530

    Add ModulesPage file

---
+++ src/pages/InstructorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function InstructorPage() {
+  return (
+    <>
+    <div style={{ width: "1000px", height: "640px", background: "black", marginLeft: "105px" }}>
+      
+    </div>
+    </>
+  )
+}
